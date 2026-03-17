d60dc24f167fbb80d919763198f170007f41f3e5
belo79226 <belo79226@edu.ucl.dk>
2026-03-17 13:43:59 +0100

    Update Program.cs

--- Repetition2/Program.cs
+++ Repetition2/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Repetition2
+namespace Repetition2
 {
     internal class Program
     {
@@ -6,11 +6,21 @@
         {
             Student student1 = new Student("Alice", 20);
             Student student2 = new Student("Bo", 23);
-            Course course1 = new Course("Matematik");
+			Student student3 = new Student("Bob", 25);
+
+
+			Course course1 = new Course("Matematik");
 
             course1.AddStudent(student1);
             course1.AddStudent(student2);
-            course1.PrintStudents();
+			course1.AddStudent(student3);
+
+			Console.WriteLine("Alle studerende:\n");
+
+			course1.PrintStudents();
+            Console.WriteLine("Alle studerende over 22 år:\n");
+            course1.PrintStudentsByAge(22);
+
             Console.ReadLine();
         }
     }
